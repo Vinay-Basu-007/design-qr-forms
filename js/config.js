9aea46d6922f8089c8e6f0eb73c4ae7ecfb6c4e5
Vinay-Basu-007 <vinay007basu.raj@gmail.com>
2026-07-07 17:24:51 +0530

    updated config file with new deployment link

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Set GOOGLE_SCRIPT_URL after deploying google-apps-script/Code.gs
 window.APP_CONFIG = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwGLZaubkaN-YQjWHPpcYx_5pKt7iNJJo8ORl5bPl4G4XFEr3sC7YMm3UCz71WkpLrw/exec",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzPXT4uQ25IaXrUSWe7qsFjKXS6THLToMIRJ9LMBMBj3VsB8B-5hRtVdjkfz8Ebq4a5/exec",
   BASE_URL: window.location.origin,
 };
