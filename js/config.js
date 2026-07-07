457995e8c3274daecb398269708551f735a873a6
Vinay-Basu-007 <vinay007basu.raj@gmail.com>
2026-07-07 19:37:33 +0530

    updated script url

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Set GOOGLE_SCRIPT_URL after deploying google-apps-script/Code.gs
 window.APP_CONFIG = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzPXT4uQ25IaXrUSWe7qsFjKXS6THLToMIRJ9LMBMBj3VsB8B-5hRtVdjkfz8Ebq4a5/exec",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzmlVz2P5mY-X-_dP1iHhfo1XL09V9PLeRImeIBUenolFFOGvqRhcDI4mnMMNNBEXZe/exec",
   BASE_URL: "https://vinay-basu-007.github.io/design-qr-forms",
 };
