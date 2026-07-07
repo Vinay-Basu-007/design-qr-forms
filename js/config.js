c4a9eaadcaead08b6f15eeb2496802bf28e736ea
Basavaraj K <95667270+Vinay-Basu-007@users.noreply.github.com>
2026-07-07 17:28:46 +0530

    Lock BASE_URL to project Pages URL

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Set GOOGLE_SCRIPT_URL after deploying google-apps-script/Code.gs
 window.APP_CONFIG = {
   GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzPXT4uQ25IaXrUSWe7qsFjKXS6THLToMIRJ9LMBMBj3VsB8B-5hRtVdjkfz8Ebq4a5/exec",
-  BASE_URL: window.location.origin,
+  BASE_URL: "https://vinay-basu-007.github.io/design-qr-forms",
 };
